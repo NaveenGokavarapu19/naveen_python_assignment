39d2e1db7f0da9f13ff6f433ee2be24f78553f7a
NaveenGokavarapu19 <gokavarapu.naveen97@gmail.com>
2022-11-19 01:36:26 +0530

    "completed writing logic for the both tables"

--- src/models.py
+++ src/models.py
@@ -1,45 +1,44 @@
 from sqlalchemy import Column, Integer, String,Sequence,ForeignKey,create_engine,BigInteger
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker,declarative_base,relationship
+
+
 Base = declarative_base()
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, Sequence("user_id_seq"), primary_key=True)
     name = Column(String(50))
-    email =  Column(String(50)) 
+    email =  Column(String(50),unique=True) 
     password =  Column(String(50)) 
     profile = Column(String(50))
     picture_url = Column(String(50))
-    mobile = Column(BigInteger)
+    mobile = Column(BigInteger,unique=True)
+    child = relationship("Address", back_populates="parent",uselist=False)
 
     def __repr__(self):
-        return "<User(name='%s', email='%s',password='%s',profile='%s',picture_url='%s',mobile='%d')>" %(
-            self.name,
-            self.email,
-            self.password,
-            self.profile,
-            self.picture_url,
-            self.mobile
-        )
+        return str("name=" + self.name + "email=" + self.email + "password=" + self.password + "profile=" +self.profile + "picture_url=" +self.picture_url + "mobile =" + self.mobile)
 
 class Address(Base):
-    __tablename__ = "users"
-    id = Column(Integer, Sequence("user_id_seq"), primary_key=True)
+    __tablename__ = "address"
+    id = Column(Integer, Sequence("address_id_seq"), primary_key=True)
     flat_number = Column(String(50))
     address1 =  Column(String(50)) 
     address2 =  Column(String(50)) 
     city = Column(String(50))
     state = Column(String(50))
     pin_code = Column(BigInteger)
+    user_mail = Column(String(50), ForeignKey("users.email"))
+    parent = relationship("User", back_populates="child")
+
 
     def __repr__(self):
-        return "<User(name='%s', flat_number = '%s',address1='%s',address2='%s',city='%s',state='%s',pin_code='%d')>" %(
+        return "<Address(flat_number = '%s',address1='%s',address2='%s',city='%s',state='%s',pin_code='%d',user_mail='%s')>" %(
             self.flat_number,
             self.address1,
             self.address2,
             self.city,
             self.state,
-            self.pin_code
+            self.pin_code,
+            self.user_email
         )
 
 
